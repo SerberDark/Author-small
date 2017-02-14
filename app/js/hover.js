776d786fffb21cd5886001b42a51568f0080fcd2
Serhii Omelianenko <serberdark@gmail.com>
2017-02-14 21:35:11 +0200

    Закончил hover

--- app/js/hover.js
+++ app/js/hover.js
@@ -1,5 +1,18 @@
 
-// $('.button-holder a').hover(
-// function(){ $('.toolkit-holder').addClass('hover') },
-// function(){ $('.toolkit-holder').removeClass('hover') }
-// );
+
+;(function ($){
+	var btn = $('.button-holder a');
+	var toolkitHolder = $('.toolkit-holder');
+
+function mouseIn(){
+	var parent =  $(this).closest('.button-holder');
+	var sib = $(parent).siblings('.toolkit-holder'); 
+	$(sib).addClass('hover');
+};
+function mouseOut(){ 
+	toolkitHolder.removeClass('hover') 
+}
+
+btn.hover(mouseIn, mouseOut);
+
+})(jQuery)
